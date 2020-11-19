d359e4a44000e995764daa2e276a5eb03e060fff
arjun <arjun123@gmail.com>
2020-11-19 17:55:20 +0530

    commit javapgm5

---
+++ src/test/java/javapgm5/inputstreamstringtest.java
@@ -0,0 +1,14 @@
+package javapgm5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class inputstreamstringtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
